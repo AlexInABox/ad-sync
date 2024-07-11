93dc2c9efb691169400af39e8456837c4760211d
AlexInABox <main@alexinabox.de>
2024-07-11 16:27:19 +0200

    add buildUserObject function

--- modules/sync.ps1
+++ modules/sync.ps1
@@ -9,66 +9,51 @@ Param(
     [bool]$debugEnabled = 1
 )
 
+#Import modules
 #import-module ActiveDirectory
+$buildUserObjectModule = Join-Path -Path $PSScriptRoot -ChildPath "buildUserObject.ps1"
+
 
 #Load config values
 $configObject = Get-Content -Path $configPath -Raw | ConvertFrom-Json
 [char]$delimiter = $configObject.csvDelimiter
-$parentDN = $configObject.parentDN
 $header = $configObject.header -split $delimiter
-[string]$defaultUserPassword = $configObject.defaultUserPassword
 
-
-
-function buildUserObject() {
+#Check if all the OUs in the path of a user exist, if not create them
+function ensureOUExists {
     param (
-        [Parameter(Mandatory = $true)]
-        [string]$mapID,
-        [Parameter(Mandatory = $true)]
-        [string]$givenName,
-        [Parameter(Mandatory = $true)]
-        [string]$location,
-        [Parameter(Mandatory = $true)]
-        [string]$surName,
-        [Parameter(Mandatory = $true)]
-        [string]$telephoneNumber,
-        [Parameter(Mandatory = $true)]
-        [string]$email
+        [string]$ouPath
     )
 
-    if ([string]::IsNullOrEmpty($email)) {
-        $email = $csvUserLine.UserPrincipalName.Split('@')[0] + "@polizei.berlin.de"
-    }
+    # Split the OU path into components
+    $ouComponents = $ouPath -split ","
 
-    $newUser = @{
-        #mandatory
-        # "Path" entspricht dem Distinguished Name, aber ohne den 'CN=24318496,'-Teil, aka "X.500 OU path"
-        # OU=LKA 725,OU=LKA 72,OU=LKA 7,OU=LKA,OU=Dienststellen,OU=Benutzer,OU=ad,DC=msd,DC=polizei,DC=berlin,DC=de
-        Path                  = $location
-        SamAccountName        = "msd$($mapID)"
-        UserPrincipalName     = "msd$($mapID)@msd.polizei.berlin.de"
-        Enabled               = $true
-        ChangePasswordAtLogon = $true
-        AccountPassword       = $(ConvertTo-SecureString $defaultUserPassword -AsPlainText -Force)
-        Name                  = $mapID
-        DisplayName           = "$($givenName) $($surName)"
-        GivenName             = $givenName
-        Surname               = $surName
-        EmailAddress          = $email
-        OfficePhone           = $csvUserLine.telephoneNumber #OBS! Get-ADUser uses telephoneNumber, Set-ADUser only knows OfficeTelephone or MobilePhone
+    # Starting from the end of the path, check each level
+    for ($i = ($ouComponents.Length - 1); $i -ge 0; $i--) {
+        $ouSubPath = ($ouComponents[0..$i] -join ",")
+        try {
+            # Check if the OU exists
+            $ouExists = Get-ADOrganizationalUnit -Filter { DistinguishedName -eq $ouSubPath } -ErrorAction Stop
+            if ($ouExists) {
+                break
+            }
+        }
+        catch {
+            # If the OU doesn't exist, create it
+            New-ADOrganizationalUnit -Name ($ouComponents[$i] -replace "^OU=", "") -Path ($ouComponents[($i + 1)..($ouComponents.Length - 1)] -join ",")
+        }
     }
-
-    return $newUser
 }
 
-#Remove the header
+#Import the CSV file with custom header
 $data = Import-Csv -Path $csvPath -Delimiter $delimiter -Header $header -Encoding UTF8
 
 #Add the default password to every user
 foreach ($user in $data) {
-    $user | Add-Member -MemberType NoteProperty -Name 'password' -Value $defaultUserPassword
-    Write-Host "User $($user.DisplayName) has been added with password $($user.password)"
+    #. $buildUserObjectModule -csvUserLine $user -configPath $configPath -debugEnabled $debugEnabled | Format-Table | Out-String | Write-Output
+    . $buildUserObjectModule -csvUserLine $user -configPath $configPath -debugEnabled $debugEnabled
+    if (-Not $debugEnabled) {
+        $path = generatePathFromCanonicalName -canonicalName $user.CanonicalName
+        ensureOUExists -ouPath $path
+    }
 }
-
-$someUser = Get-ADUser -Filter * -SearchBase $parentDN
-Write-Host $someUser

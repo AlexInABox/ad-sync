ae12fbca5a3e1ae2d5299a824d27a474a086a385
AlexInABox <main@alexinabox.de>
2024-07-29 15:58:36 +0200

    create OU path if not exisiting

--- modules/addUserObjectToAD.ps1
+++ modules/addUserObjectToAD.ps1
@@ -11,6 +11,7 @@ Param(
 
 #Load config values
 $configObject = Get-Content -Path $configPath -Raw | ConvertFrom-Json
+$debugModule = Join-Path -Path $PSScriptRoot -ChildPath "debug.ps1"
 
 #check if user already exists
 ###copy group permissions to new userObject
@@ -18,18 +19,43 @@ $configObject = Get-Content -Path $configPath -Raw | ConvertFrom-Json
 #add user to AD
 #maybe: give user group permissions
 
+
+#Check if all the OUs in the path of a user exist, if not create them
+function ensureOUExists {
+    param (
+        [string]$ouPath
+    )
+    # Split the OU path into components
+    $ouComponents = $ouPath -split ","
+    # Starting from the end of the path, check each level (-5 because we dont check the DC's only ON)
+    for ($i = ($ouComponents.Length - 5); $i -ge 0; $i--) {
+        $ouSubPath = ($ouComponents[$i..($ouComponents.Length - 1)] -join ",")
+        # Check if the OU exists
+        $ouExists = Get-ADOrganizationalUnit -Filter { DistinguishedName -eq $ouSubPath } -ErrorAction Stop
+        if ($ouExists) {
+            continue
+        }
+        # If the OU doesn't exist, create it
+        . $debugModule -message "Adding OU to AD: $($ouSubPath)" -debugEnabled $debugEnabled
+        New-ADOrganizationalUnit -Name ($ouComponents[$i] -replace "^OU=", "") -Path ($ouComponents[($i + 1)..($ouComponents.Length - 1)] -join ",")
+    }
+}
+
 function userAlreadyExists {
     $alreadyExists = 1
     try {
         Get-ADUser $userObject.Name
     }
     catch {
-        Write-Host "Yippie" $userObject.Name "is unique!!"
+        #Write-Host "Yippie" $userObject.Name "is unique!!"
         $alreadyExists = 0
     }
     return $alreadyExists
 }
 
 if (userAlreadyExists) {
+    Write-Host "User already exists"
     #TODO: Copy old permissions and then delete old user
 }
+
+ensureOUExists($userObject.path)

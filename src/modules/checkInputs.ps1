b6312865d307ce99c106c63fab8e7fea87b003d9
AlexInABox <main@alexinabox.de>
2024-11-11 16:32:47 +0100

    Check if CSV has empty fields (Added, MRX)

--- src/modules/checkInputs.ps1
+++ src/modules/checkInputs.ps1
@@ -11,8 +11,12 @@ $debugModule = Join-Path -Path $PSScriptRoot -ChildPath "debug.ps1"
 
 function checkTable {
     param (
-        [string]$path
+        [string]$path,
+        [string]$configPath
     )
+    
+    # seperator line
+    . $debugModule -message " "
 
     #Check if file exists
     if (-Not (Test-Path -Path $path -PathType Leaf)) {
@@ -27,7 +31,31 @@ function checkTable {
         return $false
     }
 
-    return $true
+    # Check if CSV has empty fields (Added, MRX)
+    $configObject = Get-Content -Path $configPath -Encoding UTF8 | ConvertFrom-Json
+    [char]$delimiter = $configObject.csvDelimiter
+    $header = $configObject.header -split $delimiter
+
+    # check content of csv
+    $csv = Import-Csv -Path $path -Delimiter $delimiter -Header $header -Encoding UTF8
+    $faultFound = $false
+    $csv = $csv[1..($csv.Length)]
+    $csv | Foreach-Object {
+        # check if name is valid (for now: length must be 8 - TODO later: only numbers)
+        if (($_.SamAccountName.Length -ne 8) -or ($_.Name.Length -ne 8)) {
+            . $debugModule -message "Error: The user $($_.SamAccountName) does not match number pattern."
+            $faultFound = $true
+        }
+        # check if all cells have content in it
+        #foreach ($property in $_.psobject.properties) {
+        #    if ([string]::IsNullOrWhiteSpace($property.Value)) {
+        #        . $debugModule -message "Error: The cell $($property.Name) was empty for $($_.SamAccountName)."
+        #        $faultFound = $true
+        #    }
+        #}
+    }
+    
+    return (-not $faultFound)
 }
 
 function testJSONKey {
@@ -74,4 +102,4 @@ function checkConfig {
     return $true
 }
 
-return (checkConfig -path $configPath) -and (checkTable -path $tablePath)
+return (checkConfig -path $configPath) -and (checkTable -path $tablePath -configPath $configPath)

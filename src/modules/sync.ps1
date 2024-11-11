7478299d438e63efac552d9ebf2e92802a6f8831
AlexInABox <main@alexinabox.de>
2024-11-11 16:38:34 +0100

    reorder debugLog, MRX

--- src/modules/sync.ps1
+++ src/modules/sync.ps1
@@ -21,7 +21,7 @@ $configObject = Get-Content -Path $configPath -Encoding UTF8 | ConvertFrom-Json
 $header = $configObject.header -split $delimiter
 
 #Import the CSV file with custom header
-$data = Import-Csv -Path $csvPath -Delimiter $delimiter -Header $header -Encoding UTF8
+$data = @(Import-Csv -Path $csvPath -Delimiter $delimiter -Header $header -Encoding UTF8)
 
 $processedUserCount = 0
 $reportProcessedUserCountInterval = [int]($data.Length / 50) #this will simulate a progress bar
@@ -29,9 +29,6 @@ if ($reportProcessedUserCountInterval -eq 0){
     $reportProcessedUserCountInterval = 1
 }
 foreach ($user in $data) {
-    if  (-Not ($processedUserCount % $reportProcessedUserCountInterval) -Or ($processedUserCount -eq $data.Length - 1)) {
-        . $debugModule -message "Processed $($processedUserCount)/$($data.Length) users."
-    }
     #. $buildUserObjectModule -csvUserLine $user -configPath $configPath -debugEnabled $debugEnabled | Format-Table | Out-String | Write-Output
     $userObject = . $buildUserObjectModule -csvUserLine $user -configPath $configPath
 
@@ -39,6 +36,10 @@ foreach ($user in $data) {
     . $addUserObjectToAD -userObject $userObject -configPath $configPath -readOnly $readOnly
 
     $processedUserCount++
+
+    if  (-Not ($processedUserCount % $reportProcessedUserCountInterval) -Or ($processedUserCount -eq $data.Length)) {
+        . $debugModule -message "Processed $($processedUserCount)/$($data.Length) users."
+    }
 }
 
 if ($readOnly) {
